8d4ff32023459f83906eeb77a42ec0342d0ab2cc
rahul <rahulkeshri09.rk@gmail.com>
2020-08-06 01:27:49 +0530

    Updates

---
+++ precache-manifest.b43e0cdd3731508b8289e7d2ea2a6a66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a440b53d33548228bd97dc76976622be",
+    "url": "/ipod/index.html"
+  },
+  {
+    "revision": "d504e2ffaa9a24a636b6",
+    "url": "/ipod/static/css/main.758459d2.chunk.css"
+  },
+  {
+    "revision": "e89ea585e3417f29d6d9",
+    "url": "/ipod/static/js/2.4e644173.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/ipod/static/js/2.4e644173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d504e2ffaa9a24a636b6",
+    "url": "/ipod/static/js/main.e3e24e7e.chunk.js"
+  },
+  {
+    "revision": "e6acf8b982710a9850e8",
+    "url": "/ipod/static/js/runtime-main.59161aa3.js"
+  }
+]);
